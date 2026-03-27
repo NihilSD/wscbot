26bac7f2e312f18018a2a6ae5176fbf7f7410fa0
NihilSineDeo <vvictis86@gmail.com>
2026-03-27 16:40:16 +0200

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,1 +1,10 @@
+module.exports = async function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const key = process.env.CLAUDE_API_KEY;
+  res.json({
+    key_set: !!key,
+    key_preview: key ? key.slice(0, 20) + '...' : 'MISSING',
+    node_version: process.version
+  });
+};
 
